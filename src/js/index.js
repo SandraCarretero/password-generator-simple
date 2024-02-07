772b167d0a408b6339e1e421df888d1fe539b191
SandraCarretero <sandracarretero24@gmail.com>
2024-02-07 16:05:29 +0100

    change js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
+import { buttonElement, rangeElement } from './dom';
+import { changeLengthText, printFinalPassword } from './functions';
 
-// Elementos del DOM
-import { buttonElement, rangeElement } from "./dom";
-import { changeLengthText, printFinalPassword } from "./functions";
-
-// Constantes de JS
-
-
-// Variables de JS
-
-// Funciones
-
-// Eventos
 rangeElement.addEventListener('input', changeLengthText);
 buttonElement.addEventListener('click', printFinalPassword);
